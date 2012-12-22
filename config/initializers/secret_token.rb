933a348a66a7eaf64268ea33b42568b1c9dbd452
Ricky Barefield <ricky.barefield@googlemail.com>
2012-12-22 18:04:53 +0000

    Added ruby project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViewFirstTodo::Application.config.secret_token = '836606b02224f7bd09fe573405002d9f3eae9d5ed59f6a747037a3e6bdba46e8a55b87e72586b503a4af86aa590e6951b8f76626be2140cc40ed03e835742554'
--- src/Todo.coffee
+++
@@ -1,8 +0,0 @@
-class window.Todo extends Spine.Model
-
-  @configure "Todo", "name", "description"
-
-
-window.Todo.extend(Spine.Model.Local);
-
-window.Todo.fetch()
